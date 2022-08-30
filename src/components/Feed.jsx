dcde9c90879ad9ec5b920fae173889021e48d7ad
unknown <matt.mcquain@gmail.com>
2022-08-29 21:52:36 -0700

    working on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+
+import { Sidebar, Videos } from './'
 
 
 const Feed = () => {
@@ -24,6 +25,34 @@ const Feed = () => {
         >
           Copyright 2022 WCC
         </Typography>
+      </Box>
+
+      {/* Videos */}
+      <Box 
+        sx={{
+          overflowY: 'auto', 
+          height: '90vh',
+          flex: 2,
+        }}
+        p={2}
+      >
+        <Typography 
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: 'white' }}
+        >      
+          New <span
+                style={{
+                  color: '#F31503'
+                }}
+              >
+              Videos
+            </span>
+        </Typography>
+
+        <Videos videos={[]} />
+
       </Box>
     </Stack>
   )
