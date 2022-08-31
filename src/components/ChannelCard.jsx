8ba921b0f657af6e9c1acbbe16dbcbc0b44eb2dd
unknown <matt.mcquain@gmail.com>
2022-08-30 21:41:49 -0700

    Got most of the styling done on the ChannelCard. DemoPfP is not displaying so need to come back to that later. Gonna start working on the Channel Page.

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
-}
+import { demoProfilePicture } from '../utils/constants'
+
+const ChannelCard = ({ channelDetail }) => (
+
+  <Box
+    sx={{
+      boxShadow: 'none',
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: { xs: '365px', md: '320px' },
+      height: '326px',
+      margin: 'auto',
+    }}
+  >
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <CardContent
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          textAlign: 'center',
+          color: '#fff',
+        }}
+      >
+        <CardMedia
+          image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+          alt={channelDetail?.snippet?.title}
+          sx={{ borderRadius: '50%', width: '180px', height: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+        />
+
+        <Typography variant='h6'>
+          {channelDetail?.snippet?.title}
+          <CheckCircle 
+            sx={{ fontSize: 14, color: 'gray', ml: '5px' }}
+          />
+        </Typography>
+
+        {channelDetail?.statistics?.subscriberCount && (
+          <Typography variant='subtitle2' color='gray'>
+            {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Subscribers
+          </Typography>
+        )}
+      </CardContent>
+
+    </Link>
+  </Box>
+)
 
 export default ChannelCard
