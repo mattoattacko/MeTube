5bb9f95d35e351fe098b2329fe29e3e08684f4a9
unknown <matt.mcquain@gmail.com>
2022-08-31 21:07:24 -0700

    app can pull videos from the api correctly. Working on Channel Detail component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'  //we use the 'useParams' hook to get the channel ID string from the URL
+import { Box } from '@mui/material'
+
+import { Videos, ChannelCard } from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
+
 
 const ChannelDetail = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  const { id } = useParams()  
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items))
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+      <Box>
+      <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }}           
+      />
+      </Box>
+    </Box>
   )
 }
+
 
 export default ChannelDetail
